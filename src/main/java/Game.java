d6dc55974c59fad01d00afd47b4a67f5bb8a5a62
jennkimm <smartshk98@gmail.com>
2026-05-28 14:20:41 +0900

    [feature] 49

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,16 @@
 public class Game {
-    public void guess(String guessNumber) {
+    public String question;
+
+    public GuessResult guess(String guessNumber) {
+        assertIllegalArgument(guessNumber);
+        if (guessNumber.equals(question)) {
+            return new GuessResult(true, 3, 0);
+        } else {
+            return null;
+        }
+    }
+
+    private void assertIllegalArgument(String guessNumber) {
         if (guessNumber == null) {
             throw new IllegalArgumentException();
         }
@@ -11,11 +22,14 @@ public class Game {
                 throw new IllegalArgumentException();
             }
         }
-
-        if (guessNumber.charAt(0) == guessNumber.charAt(1) ||
-                guessNumber.charAt(0) == guessNumber.charAt(2) ||
-                guessNumber.charAt(1) == guessNumber.charAt(2)) {
+        if (isDuplicatedNumber(guessNumber)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private static boolean isDuplicatedNumber(String guessNumber) {
+        return guessNumber.charAt(0) == guessNumber.charAt(1) ||
+                guessNumber.charAt(0) == guessNumber.charAt(2) ||
+                guessNumber.charAt(1) == guessNumber.charAt(2);
     }
 }
