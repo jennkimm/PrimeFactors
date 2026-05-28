c633cb1ce63926756261576de88ef39606e329e5
jennkimm <smartshk98@gmail.com>
2026-05-28 10:17:33 +0900

    [refactor] 41

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -2,16 +2,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactor {
-    public List<Integer> of(int num) {
+    public List<Integer> of(int number) {
         List<Integer> factors = new ArrayList<>();
-        for (int divisor = 2; divisor * divisor <= num; divisor++) {
-            while (num % divisor == 0) {
+        for (int divisor = 2; divisor * divisor <= number; divisor++) {
+            while (number % divisor == 0) {
                 factors.add(divisor);
-                num /= divisor;
+                number /= divisor;
             }
         }
-        if (num > 1)
-            factors.add(num);
+        if (number > 1)
+            factors.add(number);
         return factors;
     }
 }
